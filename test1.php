3bc755f17d9e93fa003a1d6bc9346840221c1610
giomoi01 <nguoihanoithamlang01@gmail.com>
2022-04-23 16:55:57 +0700

    Clear

--- test1.php
+++ test1.php
@@ -1,5 +1,6 @@
-<?php 
-if(Sua dong nay roi){
-echo 'Them file aaaa';
-}
-?>
+dòng không thay đổi
+dòng thay đổi ở test 1
+dòng không thay đổi
+dòng sẽ thay đổi ở test 1
+dòng sẽ thay đổi ở test 2
+dòng không thay đổi
--- test2.php
+++ test2.php
@@ -1,5 +1,11 @@
 <?php 
-echo 'Da thay doi dong nay';
-echo 'Them dong so 3 se giu lai dong nay';
-echo 'THem dong nua';
+echo 'Không thay đổi';
+echo 'dòng này sẽ thay đổi ở nhánh 1';
+echo 'Dòng này sẽ thay đổi ở nhánh 2';
+echo 'không thay đổi';
+echo 'Dòng sẽ thay đổi ở nhánh 1';
+echo 'dòng ko thay đổi';
+echo 'dòng sẽ thay đổi ở nhánh 2';
+echo 'dòng ko thay đổi';
+echo 'dòng thay đổi ở nhánh 1';
 ?>
