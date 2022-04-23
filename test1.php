de47af02bf097f56bb27716a27aa058e07df4976
giomoi01 <nguoihanoithamlang01@gmail.com>
2022-04-23 17:04:13 +0700

    Đã thay đổi test 1

--- test1.php
+++ test1.php
@@ -1,6 +1,9 @@
 dòng không thay đổi
-dòng thay đổi ở test 1
+dòng thay đổi ở test 1 OK tôi đã thay đổi
+và thêm 1 dòng nữa(test 1)
 dòng không thay đổi
-dòng sẽ thay đổi ở test 1
+Dòng thay đổi thêm (test 1)
+ Ok tôi đã thay đổi (test 1)
+ Tôi đã thêm 3 dòng (test 1)
 dòng sẽ thay đổi ở test 2
 dòng không thay đổi
--- test2.php
+++ test2.php
@@ -1,11 +1,17 @@
 <?php 
 echo 'Không thay đổi';
-echo 'dòng này sẽ thay đổi ở nhánh 1';
+for($i=1;$i<$n;$i++){
+    echo 'Test 1 đã thay đổi thành công';
+    echo 'Test 1.1 đã thay đổi thành công';
+}
 echo 'Dòng này sẽ thay đổi ở nhánh 2';
 echo 'không thay đổi';
-echo 'Dòng sẽ thay đổi ở nhánh 1';
+echo 'Test 1.1 đã thay đổi thành công';
+echo 'Test 1.2 đã thay đổi thành công';
+echo 'Test 1.3 đã thay đổi thành công';
 echo 'dòng ko thay đổi';
 echo 'dòng sẽ thay đổi ở nhánh 2';
 echo 'dòng ko thay đổi';
-echo 'dòng thay đổi ở nhánh 1';
+echo 'Test 1.4 đã thay đổi thành công';
+echo 'Test 1.5 đã thay đổi thành công';
 ?>
