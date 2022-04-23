ba2f6c079a66044b1bd4e449930106e9c6c8ed9a
giomoi01 <nguoihanoithamlang01@gmail.com>
2022-04-23 17:07:48 +0700

    Đã thay đổi test 2

--- test2.php
+++ test2.php
@@ -1,11 +1,16 @@
 <?php 
 echo 'Không thay đổi';
 echo 'dòng này sẽ thay đổi ở nhánh 1';
-echo 'Dòng này sẽ thay đổi ở nhánh 2';
+if($i=0){
+    echo 'Nội dung nhánh 2 đã thay đổi (test2)';
+}
 echo 'không thay đổi';
 echo 'Dòng sẽ thay đổi ở nhánh 1';
 echo 'dòng ko thay đổi';
-echo 'dòng sẽ thay đổi ở nhánh 2';
+if($i=2){
+    echo 'Nội dung nhánh 2 đã thay đổi (test2)';
+    echo 'Nội dung nhánh 2.1 đã thay đổi (test2)';
+}
 echo 'dòng ko thay đổi';
 echo 'dòng thay đổi ở nhánh 1';
 ?>
