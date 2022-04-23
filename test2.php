427f6d0753acda9808a4fc59dc5a268ab787b6ad
giomoi01 <nguoihanoithamlang01@gmail.com>
2022-04-23 20:04:32 +0700

    clear all

--- test2.php
+++ test2.php
@@ -1,22 +1,9 @@
 <?php 
 echo 'Không thay đổi';
-for($i=1;$i<$n;$i++){
-    echo 'Test 1 đã thay đổi thành công';
-    echo 'Test 1.1 đã thay đổi thành công';
-}
-if($i=0){
-    echo 'Nội dung nhánh 2 đã thay đổi (test2)';
-}
-echo 'không thay đổi';
-echo 'Test 1.1 đã thay đổi thành công';
-echo 'Test 1.2 đã thay đổi thành công';
-echo 'Test 1.3 đã thay đổi thành công';
+echo 'Se thay đổi 1';
+echo 'Sẽ thay đổi 2';
+echo 'Sẽ thay đổi 1 và 2';
 echo 'dòng ko thay đổi';
-if($i=2){
-    echo 'Nội dung nhánh 2 đã thay đổi (test2)';
-    echo 'Nội dung nhánh 2.1 đã thay đổi (test2)';
-}
-echo 'dòng ko thay đổi';
-echo 'Test 1.4 đã thay đổi thành công';
-echo 'Test 1.5 đã thay đổi thành công';
+echo 'Sẽ thay đổi 1 và 2';
+echo 'Sẽ thay đổi 1';
 ?>
