c417a44c879c0d6ecb2ec64ab1e4fc3993039e9a
giomoi01 <nguoihanoithamlang01@gmail.com>
2022-04-24 09:46:06 +0700

    update 3

---
+++ test3.php
@@ -0,0 +1,25 @@
+<?php 
+echo 'Không thay đổi';
+if($i=1){
+    echo 'Da thay doi o day roi1';
+    echo 'Da thay doi o day roi1';
+    echo 'Da thay doi o day roi1';
+    echo 'Da thay doi o day roi1';
+}
+echo 'Sẽ thay đổi 2';
+if($i==1){
+    echo 'Da thay doi o day roi1';
+    echo 'Da thay doi o day roi1';
+    echo 'Da thay doi o day roi1';
+}
+echo 'dòng ko thay đổi';
+if($i==1){
+    echo 'Da thay doi o day roi1';
+    echo 'Da thay doi o day roi1';
+}
+if($i=1){
+    echo 'Da thay doi o day roi1';
+    echo 'Da thay doi o day roi1';
+    echo 'Da thay doi o day roi1';
+}
+?>
