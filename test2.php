9485280c763f4e34db066c1adb73162a7242ecfb
giomoi01 <nguoihanoithamlang01@gmail.com>
2022-04-24 11:36:43 +0700

    chi can 1 lenh
    chi can 1 lenh
    
    chi can 1 lenh

--- test2.php
+++ test2.php
@@ -6,35 +6,6 @@ if($i=1){
     echo 'Da thay doi o day roi1';
     echo 'Da thay doi o day roi1';
 }
-if($c=2){
-    echo 'Da thay đổi 2'; 
-    echo 'Da thay đổi 2'; 
-    echo 'Da thay đổi 2'; 
-}
-if($i==1){
-    echo 'Da thay doi o day roi1';
-    echo 'Da thay doi o day roi1';
-    echo 'Da thay doi o day roi1';
-}
-if($c=2){
-    echo 'Da thay đổi 21'; 
-    echo 'Da thay đổi 21'; 
-    echo 'Da thay đổi 21'; 
-}
-echo 'dòng ko thay đổi';
-if($i==1){
-    echo 'Da thay doi o day roi1';
-    echo 'Da thay doi o day roi1';
-    echo 'Da thay doi o day roi1';
-}
-if($c=2){
-    echo 'Da thay đổi 21'; 
-    echo 'Da thay đổi 21'; 
-    echo 'Da thay đổi 21'; 
-}
-if($i=1){
-    echo 'Da thay doi o day roi1';
-    echo 'Da thay doi o day roi1';
-    echo 'Da thay doi o day roi1';
-}
+
+
 ?>
