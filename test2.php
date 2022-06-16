d330fd2e796fa814d85a21aa9d654522ac658d89
giomoi01 <nguoihanoithamlang01@gmail.com>
2022-06-16 15:41:30 +0700

    Add 4 file

--- test2.php
+++ test2.php
@@ -5,6 +5,4 @@ if($i=1){
     echo 'Da thay doi o day roi1';
     echo 'Da thay doi o day roi1';
 }
-
-
 ?>
--- test3.php
+++ test3.php
@@ -6,20 +6,5 @@ if($i=1){
     echo 'Da thay doi o day roi1';
     echo 'Da thay doi o day roi1';
 }
-echo 'Sẽ thay đổi 2';
-if($i==1){
-    echo 'Da thay doi o day roi1';
-    echo 'Da thay doi o day roi1';
-    echo 'Da thay doi o day roi1';
-}
-echo 'dòng ko thay đổi';
-if($i==1){
-    echo 'Da thay doi o day roi1';
-    echo 'Da thay doi o day roi1';
-}
-if($i=1){
-    echo 'Da thay doi o day roi1';
-    echo 'Da thay doi o day roi1';
-    echo 'Da thay doi o day roi1';
-}
+
 ?>
